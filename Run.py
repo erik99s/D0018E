9baf4b17fecd9d661f213e142bf5808164fb982d
praqma <praqma@localhost.localdomain>
2020-11-07 19:16:02 +0100

    base of Run.py

--- Run.py
+++ Run.py
@@ -1,1 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("HomePage.html")
+
+if __name__ == "__main__":
+    app.run()
